26643e02cb1ba4d7689f49eca7fcca5bdb6e495e
erib <eri-b@users.noreply.github.com>
2019-05-28 15:28:39 -0400

    gameplay

--- lib/main.rb
+++ lib/main.rb
@@ -5,55 +5,57 @@ class Game
   end
 
 =begin
-
 board
 11 12 13
 21 22 23
 31 32 33
-
 =end
 
-  def moveX(pos)
-    if valid?(pos)
-      if empty?(pos)
-        @board[pos] = "X"
-        puts @board
-      else
-        puts "not empty"
-      end
+  def move(player)
+    print "Player #{player}'s turn. Position of move: "
+    pos = gets.chomp
+    if valid?(pos) && empty?(pos)
+      @board[pos] = player
+      puts @board
+    elsif !valid?(pos)
+      puts "Format incorrect"
+      move(player)
+    elsif valid?(pos) && !empty?(pos)
+      puts "Board position already full"
+      move(player)
     else
-      puts "invalid entry"
-    end
-  end
-
-  def moveO(pos)
-    if valid?(pos)
-      if empty?(pos)
-        @board[pos] = "O"
-        puts @board
-      else
-        puts "not empty"
-      end
-    else
-      puts "invalid entry"
+      puts "something is wrong"
+      move(player)
     end
   end
 
   def valid?(pos)
-    true if pos =~ /^[1-3][1-3]$/ 
+    true if pos =~ /^[1-3][1-3]$/
   end
 
   def empty?(pos)
     true if @board[pos] == nil
   end
 
+  def start
+    counter = 0
+    gameOn = true
+    while gameOn
+
+      move("X") if counter % 2 == 0
+      move("O") if counter % 2 == 1
+
+      counter += 1
+
+      gameOn = false if counter >= 4
+    end
+    puts "game over"
+  end
+
+
+
 end
 
-game1 = Game.new
 
-game1.moveX("11")
-game1.moveO("12")
-game1.moveX("21")
-game1.moveO("31")
-game1.moveO("55")
-game1.moveX("11")
+game1 = Game.new
+game1.start
