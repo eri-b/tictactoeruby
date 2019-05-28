56af4af689d3c514a077b4a7181f183ef3c5d461
DevMFernandes <contato@marcos-fernandes.com>
2019-05-28 15:38:37 -0300

    entry error handling

--- lib/main.rb
+++ lib/main.rb
@@ -1,4 +1,3 @@
-
 class Game
 
   def initialize
@@ -15,15 +14,38 @@ board
 =end
 
   def moveX(pos)
-    @board[pos] = "X"
-    puts @board
+    if valid?(pos)
+      if empty?(pos)
+        @board[pos] = "X"
+        puts @board
+      else
+        puts "not empty"
+      end
+    else
+      puts "invalid entry"
+    end
   end
 
   def moveO(pos)
-    @board[pos] = "O"
-    puts @board
+    if valid?(pos)
+      if empty?(pos)
+        @board[pos] = "O"
+        puts @board
+      else
+        puts "not empty"
+      end
+    else
+      puts "invalid entry"
+    end
   end
 
+  def valid?(pos)
+    true if pos =~ /^[1-3][1-3]$/ 
+  end
+
+  def empty?(pos)
+    true if @board[pos] == nil
+  end
 
 end
 
@@ -33,3 +55,5 @@ game1.moveX("11")
 game1.moveO("12")
 game1.moveX("21")
 game1.moveO("31")
+game1.moveO("55")
+game1.moveX("11")
