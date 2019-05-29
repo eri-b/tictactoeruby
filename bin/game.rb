635ca1e159375658027e83c180cadd9a04a94317
erib <eri-b@users.noreply.github.com>
2019-05-29 11:00:12 -0400

    multiplayer

--- bin/game.rb
+++ bin/game.rb
@@ -3,17 +3,26 @@ require '../lib/main.rb'
 class Setup
     def initialize
     end
-  
+
     def start
       puts "Welcome to Tic Tac Toe"
       print "Start a new game by typing Start: "
       com = gets.chomp.downcase
       if com == "start"
         print "Choose Single Player (1) or Multi Player (2): "
-        
+        choice = gets.chomp
+        while choice != "1" && choice != "2"
+          puts "Please enter 1 or 2: "
+          choice = gets.chomp
+        end
+        if choice == "1"
+          game2 = AiGame.new
+          game2.start
+        elsif choice == "2"
+          game1 = Game.new
+          game1.start
+        end
 
-        game1 = Game.new
-        game1.start
         print "Well done, would you like to play again? (yes / no) "
         answer = gets.chomp.downcase
         if answer == "yes"
@@ -26,8 +35,8 @@ class Setup
         puts "Alright see ya later."
       end
     end
-  
+
   end
-  
+
   start_game = Setup.new
   start_game.start
