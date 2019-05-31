d04d00da368ac5b452273409126f8c2eb7ff85cb
erib <eri-b@users.noreply.github.com>
2019-05-31 09:38:26 -0400

    game start

--- bin/tictactoe.rb
+++ bin/tictactoe.rb
@@ -1,10 +1,13 @@
-require_relative '../lib/board.rb'
-require_relative '../lib/game.rb'
-require_relative '../lib/player.rb'
+# require_relative '../lib/board.rb'
+# require_relative '../lib/game.rb'
+# require_relative '../lib/player.rb'
+require_relative '../lib/ui'
+extend Ui
+settings = start
 
-player1 = Player.new()
-player2 = Player.new()
+player1 = Player.new(settings[1], "X")
+player2 = Player.new(settings[2], "O")
 new_board = Board.new()
 
 game = Game.new(player1, player2, board)
-game.start
+game.start if settings[0]
